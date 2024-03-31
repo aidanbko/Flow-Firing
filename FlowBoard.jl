3f7abaa490c73e201fe98d079e0fc49baea7d147
Aidan Bucko <142947955+aidanbko@users.noreply.github.com>
2024-03-31 14:49:19 -0600

    Add files via upload

--- FlowBoard.jl
+++ FlowBoard.jl
@@ -8,8 +8,11 @@ function endConfigurationsCount(n::Int)
 
     primeMatrix = generatePrimes(boardLength)
 
+    # Final configurations
     terminatedConfigs = Set{UInt128}()
     intermediateConfigs = Set{UInt128}()
+
+    # Intermediate configurations
     boardsToFire = Queue{Matrix{Int8}}()
     valuesToFire = Queue{UInt128}()
     
@@ -29,8 +32,11 @@ function endConfigurationsCount(n::Int)
                 continue
             end
 
+            # This method determines whether or not a firing move can occur
+            # If it can, then add the resultant board to the queue
             function enqueueIfNotDuplicate(from::Int, to::Int)
                 if (board[from] > board[to] + 1) # Criteria for firing
+                    # A firing move has been found, so the board is not terminated
                     isTerminated = false
                     newValue = (value ÷ primeMatrix[from]) * primeMatrix[to]
                     oldSize = length(intermediateConfigs)
@@ -48,6 +54,7 @@ function endConfigurationsCount(n::Int)
                 end
             end
 
+            # Each of the neighbors of the 'i' position
             enqueueIfNotDuplicate(i, i - size(board, 1)) # Above
             enqueueIfNotDuplicate(i, i + size(board, 1)) # Below
             enqueueIfNotDuplicate(i, i - 1) # Left
@@ -63,6 +70,25 @@ function endConfigurationsCount(n::Int)
     return length(terminatedConfigs)
 end
 
+# Generates a board with the same dimensions as those used in the above method
+# Each entry is a unique prime number starting with a 2 in the center
+# This allows for each board to be represented by a single integer using the following scheme
+#
+# Given the below intermediate configuration,
+# 0 1 0
+# 0 2 0
+# 0 0 0
+#
+# Suppose the prime matrix is as follows
+# 17 5  19
+# 3  2  7
+# 13 11 23
+#
+# Then for each face in the intermediate configuration, raise each corresponding entry to the power of the value of that face
+# Then, multiply the values for all such faces together like so
+# (17^0) * (5^1) * (19^0) * (3^0) * (2^2) * (7^0) * (13^0) * (11^0) * (23*0) = (5^1) * (2^2) = 20
+# Then by the fundamental theorem of algebra, this number corresponds uniquely to the given intermediate configuration
+#
 function generatePrimes(boardLength::Int)
 
     primeMatrix = zeros(UInt128, boardLength, boardLength)
@@ -92,8 +118,8 @@ function generatePrimes(boardLength::Int)
             end
         end
 
-        fillPositionIfEmpty(i - size(board, 1)) # Above
-        fillPositionIfEmpty(i + size(board, 1)) # Below
+        fillPositionIfEmpty(i - size(primeMatrix, 1)) # Above
+        fillPositionIfEmpty(i + size(primeMatrix, 1)) # Below
         fillPositionIfEmpty(i - 1) # Left
         fillPositionIfEmpty(i + 1) # Right
     end
@@ -101,6 +127,6 @@ function generatePrimes(boardLength::Int)
     return primeMatrix
 end
 
-for n in 1:13
-    @time println("n=", n, " ", endConfigurationsCount(n))
+for n in 1:15
+    println("n=" * string(n) * " " * string(endConfigurationsCount(n)))
 end
